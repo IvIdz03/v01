31825ac101701e219b29109491c686508f077f78
IvIdz03 <ividzano@vsite.hr>
2025-05-20 18:43:35 +0200

    implementirani kodovi

--- app/app.cpp
+++ app/app.cpp
@@ -1,11 +1,45 @@
 #include "app.h"
 #include <format>
+#include <ostream>
+#include <iomanip>
 
 namespace vsite::oop::v1
 {
-/*
-* function implementations
-*/
+    /*
+    * function implementations
+    */
+    std::string to_hex(int a) {
+        return std::format("{:X}", a);
+    }
+    std::string to_exp(double b) {
+        return std::format("{:.2e}", b);
+    }
+    void mult_table(int c, std::ostream& os) {
+
+        if (c > 0 && c <= 20) {
+
+            for (int j = 0; j <= c; j++) {
+                if (j == c) {
+                    os << j;
+                    break;
+                }
+                os << j << std::setw(4);
+            }
+            os << '\n';
+
+            for (int i = 1; i <= c; i++) {
+                os << i;
+                for (int j = 1; j <= c; j++) {
+                    os << std::setw(4) << i * j;
+                }
+                os << '\n';
+            }
+        }
+        return;
+    }
+
+
+
+
 
 } // namespace
-
